ab1a412b77da2680c2abcfcd67c6f84a88ab9a02
Fahdlabba <74735730+Fahdlabba@users.noreply.github.com>
2021-05-28 23:50:03 +0100

    submit commit

---
+++ index.js
@@ -0,0 +1,7 @@
+function verif(){
+    let inpt=document.querySelector('#ipn');
+    let email=document.querySelector('#ipe');
+    let msg=document.querySelector('#ipmsg');
+    if(!inpt.value){alert("Saisie Votre Nom");return false};    
+
+}
