a40f67ce557e5c16f2a15d9af189fbc16f16b039
Fahdlabba <74735730+Fahdlabba@users.noreply.github.com>
2021-08-25 12:11:52 +0100

    initial commit

--- index.js
+++ index.js
@@ -3,4 +3,6 @@ function btn(){
     document.querySelector("#ft").style.backgroundColor = "black";
     document.querySelector("#skills").style.backgroundColor = "black";
     document.querySelector("#contact").style.backgroundColor = "black";
+    document.querySelector("#btn3").style.backgroundColor = "black";
+
 }
